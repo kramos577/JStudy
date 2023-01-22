7154197b5df93aef590350c91ae4a07aacab95e0
Cake <kramos577@gmail.com>
2023-01-21 23:21:15 -0800

    Create Navbar Component
    This still heavily uses the original code from MaterialUI.
    This will be broken out and refined with time.

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+
+function Navbar() {
+    return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Temp Header
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            <ListItem key="Genki" disablePadding>
+            <ListItemButton component={Link} to="/genki">
+                <ListItemIcon>
+                    <MailIcon />
+                </ListItemIcon>
+                <ListItemText primary="Genki"/>
+            </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Typography paragraph>
+          This is where really cool daily things will appear!
+        </Typography>
+        <Typography paragraph>
+          Honestly, this is where they will probably go :)
+        </Typography>
+      </Box>
+    </Box>
+    )
+}
+
+export default Navbar
