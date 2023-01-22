ecb3b53807d176607c4418b5049ceb9be101676a
Cake <kramos577@gmail.com>
2023-01-21 22:25:26 -0800

    Create and Add AppRouter to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AppRouter from './AppRouter';
+
 function App() {
   return (
     <div className="App">
+      <AppRouter/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
