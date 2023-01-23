e294bf734543ce2c83192726d153f0bbff5ef4b7
Cake <kramos577@gmail.com>
2023-01-22 17:33:50 -0800

    Do Routing Better
    Moved all the app routing in the app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import AppRouter from './AppRouter';
+import Navbar from './components/Navbar';
+import Genki from './Genki';
+import Homepage from './Homepage';
+
+// Router should live in app since I'm not doing any complicated routing
+// Navbar needs to be in browser router because of all routing must happen in a routing
 
 function App() {
   return (
     <div className="App">
-      <AppRouter/>
+      <BrowserRouter>
+      
+      <Navbar/>
+          <Routes>
+              <Route path="/" element={
+                  <Homepage/>
+              }/>
+              
+              <Route path="/genki" element={
+                  <Genki/>
+              }/>
+          </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-import Navbar from "./components/Navbar";
-
 function Homepage() {
     return (
         <div>
-            <Navbar/>
             Home Page!
         </div>
     )
