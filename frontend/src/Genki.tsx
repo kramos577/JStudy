e3cec767bbdd2a0ab60edd178b1ce799d0c450e3
Cake <kramos577@gmail.com>
2023-01-22 18:21:58 -0800

    Update Page Contents

--- frontend/src/Genki.tsx
+++ frontend/src/Genki.tsx
@@ -1,9 +1,16 @@
+import { Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
 function Genki() {
     return (
-        <div>Genki Base Page!</div>
-    )
+        <div>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar />
+                <Typography paragraph>This is where all the learning will go!</Typography>
+            </Box>
+            Genki Base Page!
+        </div>
+    );
 }
 
-export default Genki
+export default Genki;
--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -1,11 +1,17 @@
+import { Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
 function Homepage() {
     return (
         <div>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar />
+                <Typography paragraph>This is where really cool daily things will appear!</Typography>
+                <Typography paragraph>Honestly, this is where they will probably go :)</Typography>
+            </Box>
             Home Page!
         </div>
-    )
+    );
 }
 
-export default Homepage
+export default Homepage;
