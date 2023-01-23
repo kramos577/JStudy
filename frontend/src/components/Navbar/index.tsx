c6f022a645470f392f5d572f15c2a6dbbcf9dca2
Cake <kramos577@gmail.com>
2023-01-22 18:22:18 -0800

    Add Nested Nav Items and Icons

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,25 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 import AppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
+import BookIcon from "@mui/icons-material/Book";
+import Box from "@mui/material/Box";
+import { Collapse } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import HomeIcon from "@mui/icons-material/Home";
 import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const drawerWidth = 240;
 
 function Navbar() {
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (
         <Box sx={{ display: "flex" }}>
-            <CssBaseline />
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
                     <Typography variant="h6" noWrap component="div">
@@ -28,6 +37,7 @@ function Navbar() {
                     </Typography>
                 </Toolbar>
             </AppBar>
+
             <Drawer
                 variant="permanent"
                 sx={{
@@ -41,28 +51,46 @@ function Navbar() {
                         <ListItem key="home" disablePadding>
                             <ListItemButton component={Link} to="/">
                                 <ListItemIcon>
-                                    <MailIcon />
+                                    <HomeIcon />
                                 </ListItemIcon>
                                 <ListItemText primary="Home" />
                             </ListItemButton>
                         </ListItem>
 
-                        <ListItem key="genki" disablePadding>
+                        <ListItem key="genki" disablePadding onClick={handleClick}>
                             <ListItemButton component={Link} to="/genki">
                                 <ListItemIcon>
-                                    <MailIcon />
+                                    <TagFacesIcon />
                                 </ListItemIcon>
                                 <ListItemText primary="Genki" />
+                                {open ? <ExpandLess /> : <ExpandMore />}
                             </ListItemButton>
                         </ListItem>
+
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <BookIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Genki I" />
+                                </ListItemButton>
+                            </List>
+                        </Collapse>
+
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <BookIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Genki II" />
+                                </ListItemButton>
+                            </List>
+                        </Collapse>
                     </List>
                 </Box>
             </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <Toolbar />
-                <Typography paragraph>This is where really cool daily things will appear!</Typography>
-                <Typography paragraph>Honestly, this is where they will probably go :)</Typography>
-            </Box>
         </Box>
     );
 }
