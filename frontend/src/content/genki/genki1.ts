caf658865ad6f4344babb9f85109887446a495be
Cake <kramos577@gmail.com>
2023-01-28 20:27:09 -0800

    Add Content Dir for Study Pages and Started Genki 1

---
+++ frontend/src/content/genki/genki1.ts
@@ -0,0 +1,316 @@
+let genki1_content = [
+    {
+        chapter: "Chapter 1",
+        title: {
+            english: "New Friends",
+            japanese: "あたらしいともだち",
+        },
+        vocabulary: [
+            {
+                english: "um...",
+                romaji: "ano",
+                kana: "あの",
+            },
+            {
+                english: "now",
+                romaji: "ima",
+                kana: "いま",
+            },
+            {
+                english: "English (language)",
+                romaji: "eego",
+                kana: "えいご",
+            },
+            {
+                english: "yes",
+                romaji: "ee",
+                kana: "ええ",
+            },
+            {
+                english: "student",
+                romaji: "gakusee",
+                kana: "がくせい",
+            },
+            {
+                english: "...language",
+                romaji: "...go",
+                kana: "〜ご",
+            },
+            {
+                english: "high school",
+                romaji: "kookoo",
+                kana: "こうこう",
+            },
+            {
+                english: "P.M.",
+                romaji: "gogo",
+                kana: "ごご",
+            },
+            {
+                english: "A.M.",
+                romaji: "gozen",
+                kana: "ごぜん",
+            },
+            {
+                english: "...years old",
+                romaji: "...sai",
+                kana: "〜さい",
+            },
+            {
+                english: "Mr./Ms....",
+                romaji: "...san",
+                kana: "〜さん",
+            },
+            {
+                english: "o'clock",
+                romaji: "...ji",
+                kana: "〜じ",
+            },
+            {
+                english: "...people",
+                romaji: "...jin",
+                kana: "〜じん",
+            },
+            {
+                english: "major",
+                romaji: "senkoo",
+                kana: "せんこう",
+            },
+            {
+                english: "teacher; Professor...",
+                romaji: "sensee",
+                kana: "せんせい",
+            },
+            {
+                english: "That's right.",
+                romaji: "soo desu",
+                kana: "そうです",
+            },
+            {
+                english: "I see.; Is that so?",
+                romaji: "soo desu ka",
+                kana: "そうですか",
+            },
+            {
+                english: "college; university",
+                romaji: "daigaku",
+                kana: "だいがく",
+            },
+            {
+                english: "telephone",
+                romaji: "denwa",
+                kana: "でんわ",
+            },
+            {
+                english: "friend",
+                romaji: "tomodachi",
+                kana: "ともだち",
+            },
+            {
+                english: "name",
+                romaji: "namae",
+                kana: "なまえ",
+            },
+            {
+                english: "what",
+                romaji: "nan/nani",
+                kana: "なん／なに",
+            },
+            {
+                english: "Japan",
+                romaji: "Nihon",
+                kana: "にほん",
+            },
+            {
+                english: "...year student",
+                romaji: "...nensee",
+                kana: "〜ねんせい",
+            },
+            {
+                english: "yes",
+                romaji: "hai",
+                kana: "はい",
+            },
+            {
+                english: "half",
+                romaji: "han",
+                kana: "はん",
+            },
+            {
+                english: "number",
+                romaji: "bangoo",
+                kana: "ばんごう",
+            },
+            {
+                english: "international student",
+                romaji: "ryuugakusee",
+                kana: "りゅうがくせい",
+            },
+            {
+                english: "I",
+                romaji: "watashi",
+                kana: "わたし",
+            },
+            {
+                english: "U.S.A",
+                romaji: "Amerika",
+                kana: "アメリカ",
+            },
+            {
+                english: "Britain",
+                romaji: "Igirisu",
+                kana: "イギリス",
+            },
+            {
+                english: "Australia",
+                romaji: "Oosutoraria",
+                kana: "オーストラリア",
+            },
+            {
+                english: "Korea",
+                romaji: "Kankoku",
+                kana: "かんこく",
+            },
+            {
+                english: "Sweden",
+                romaji: "Suweeden",
+                kana: "スウェーデン",
+            },
+            {
+                english: "China",
+                romaji: "Chuugoku",
+                kana: "ちゅうごく",
+            },
+            {
+                english: "science",
+                romaji: "kagaku",
+                kana: "かがく",
+            },
+            {
+                english: "Asian studies",
+                romaji: "ajia kenkyuu",
+                kana: "アジアけんきゅう",
+            },
+            {
+                english: "economics",
+                romaji: "keezai",
+                kana: "けいざい",
+            },
+            {
+                english: "international relations",
+                romaji: "kokusaikankee",
+                kana: "こくさいかんけい",
+            },
+            {
+                english: "computer",
+                romaji: "konpyuutaa",
+                kana: "コンピューター",
+            },
+            {
+                english: "anthropology",
+                romaji: "jinruigaku",
+                kana: "じんるいがく",
+            },
+            {
+                english: "politics",
+                romaji: "seeji",
+                kana: "せいじ",
+            },
+            {
+                english: "business",
+                romaji: "bijinesu",
+                kana: "ビジネス",
+            },
+            {
+                english: "literature",
+                romaji: "bungaku",
+                kana: "ぶんがく",
+            },
+            {
+                english: "history",
+                romaji: "rekishi",
+                kana: "れきし",
+            },
+            {
+                english: "job; work; occupation",
+                romaji: "shigoto",
+                kana: "しごと",
+            },
+            {
+                english: "doctor",
+                romaji: "isha",
+                kana: "いしゃ",
+            },
+            {
+                english: "office worker",
+                romaji: "kaishain",
+                kana: "かいしゃいん",
+            },
+            {
+                english: "high school student",
+                romaji: "kookoosee",
+                kana: "こうこうせい",
+            },
+            {
+                english: "housewife",
+                romaji: "shufu",
+                kana: "しゅふ",
+            },
+            {
+                english: "graduate student",
+                romaji: "daigakuinsee",
+                kana: "だいがくいんせい",
+            },
+            {
+                english: "college student",
+                romaji: "daigakusee",
+                kana: "だいがくせい",
+            },
+            {
+                english: "lawyer",
+                romaji: "bengoshi",
+                kana: "べんごし",
+            },
+            {
+                english: "mother",
+                romaji: "okaasan",
+                kana: "おかあさん",
+            },
+            {
+                english: "father",
+                romaji: "otoosan",
+                kana: "おとうさん",
+            },
+            {
+                english: "older sister",
+                romaji: "oneesan",
+                kana: "おねえさん",
+            },
+            {
+                english: "older brother",
+                romaji: "oniisan",
+                kana: "おにいさん",
+            },
+            {
+                english: "younger sister",
+                romaji: "imooto",
+                kana: "いもうと",
+            },
+            {
+                english: "younger brother",
+                romaji: "otooto",
+                kana: "おとうと",
+            },
+        ],
+        grammar: [
+            {
+                title: "~tara",
+                usage: "if and when",
+                notes: "this is a grammar",
+                examples: "Examples to come soon",
+            },
+        ],
+    },
+];
+
+export default genki1_content;
