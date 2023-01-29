97a54f10ba40ef54e46c5ea899e6ffc0bc5f3e1f
Cake <kramos577@gmail.com>
2023-01-29 14:56:42 -0800

    Styled Page Content
    Lots of hard coded values, but it works!

--- frontend/src/pages/Genki1Study.tsx
+++ frontend/src/pages/Genki1Study.tsx
@@ -1,4 +1,4 @@
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Toolbar, Typography } from "@mui/material";
 import genki1_content from "../content/genki/genki1";
 
 function Genki1Study() {
@@ -6,17 +6,33 @@ function Genki1Study() {
         <div>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <Toolbar />
-                <Typography paragraph>Genki I Study Page!</Typography>
-
                 {genki1_content.map((x) => (
-                    <Typography paragraph>
-                        {x.chapter} {x.title.english} {x.title.japanese}
-                        {x.vocabulary.map((y) => (
-                            <Typography paragraph>
-                                {y.english} {y.romaji} {y.kana}
+                    <Box ml={25}>
+                        <Typography variant="h3" align="left" sx={{ color: "#534D8A" }}>
+                            {x.chapter}
+                        </Typography>
+                        <Typography variant="h4" align="left" sx={{ color: "#AC80AF" }}>
+                            {x.title.japanese} - {x.title.english}
+                        </Typography>
+                        <Box mt={3}>
+                            <Typography variant="h5" align="left" sx={{ color: "#534D8A" }}>
+                                Vocabulary
                             </Typography>
-                        ))}
-                    </Typography>
+                            <Grid container spacing={2}>
+                                {x.vocabulary.map((y) => (
+                                    <Grid item>
+                                        <Card sx={{ width: 200 }}>
+                                            <CardContent>
+                                                <Typography>{y.english}</Typography>
+                                                <Typography>{y.kana}</Typography>
+                                                <Typography>{y?.romaji}</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Box>
+                    </Box>
                 ))}
             </Box>
         </div>
