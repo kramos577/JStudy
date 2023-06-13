644ca3bdd4f7d7353f9bca37a8cf8bad43cecb5d
Cake <kramos577@gmail.com>
2023-06-12 17:20:50 -0700

    Update Study Page to Use Vocab Component

--- frontend/src/pages/Genki1Study.tsx
+++ frontend/src/pages/Genki1Study.tsx
@@ -1,18 +1,12 @@
-import * as React from "react";
-import { Box, Card, CardContent, Grid, Toolbar, Typography, Switch, FormControlLabel, FormGroup } from "@mui/material";
+import { Box, Card, CardContent, Grid, Toolbar, Typography } from "@mui/material";
 import contentCh1 from "../content/genki/genki1/ch1";
 import contentCh2 from "../content/genki/genki1/ch2";
 import contentCh3 from "../content/genki/genki1/ch3";
+import Vocab from "../components/Vocab";
 
 const contentGenki1 = [contentCh1, contentCh2, contentCh3];
 
 function Genki1Study() {
-    const [checked, setChecked] = React.useState(false);
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-        console.log("checked is " + event.target.checked);
-    };
     return (
         <div>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
@@ -25,32 +19,7 @@ function Genki1Study() {
                         <Typography variant="h4" align="left" sx={{ color: "#AC80AF" }}>
                             {x.title.japanese.kanji || x.title.japanese.kana} - {x.title.english}
                         </Typography>
-                        <Box mt={3}>
-                            <Typography variant="h5" align="left" sx={{ color: "#534D8A" }}>
-                                Vocabulary
-                            </Typography>
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={<Switch color="warning" checked={checked} onChange={handleChange} />}
-                                    label="Study Mode"
-                                />
-                            </FormGroup>
-
-                            <Grid container spacing={2}>
-                                {x.vocabulary.map((y) => (
-                                    <Grid item>
-                                        <Card sx={{ width: 220 }}>
-                                            <CardContent>
-                                                <Typography>{y.english}</Typography>
-                                                <Typography>{y.kana}</Typography>
-                                                {/* <Typography>{y?.kanji}</Typography> */}
-                                                <Typography>{y?.romaji}</Typography>
-                                            </CardContent>
-                                        </Card>
-                                    </Grid>
-                                ))}
-                            </Grid>
-                        </Box>
+                        <Vocab vocabList={x.vocabulary}></Vocab>
                         <Box mt={8}>
                             <Typography variant="h5" align="left" mb={1} sx={{ color: "#534D8A" }}>
                                 Grammar
