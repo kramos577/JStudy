b6987e1cb0c377780046315fe7ae346bbcd5e33a
Cake <kramos577@gmail.com>
2023-07-13 12:59:30 -0700

    Fix Homepage Styling and Updated Content
    Also added a link to the most worked on page

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,15 +1,19 @@
-import { Box, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { Box, Link, Toolbar, Typography } from "@mui/material";
 
 function Homepage() {
     return (
         <div>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Box component="main" sx={{ width: "85%", float: "right" }}>
                 <Toolbar />
-                <Typography paragraph>This is where really cool daily things will appear!</Typography>
-                <Typography paragraph>Honestly, this is where they will probably go :)</Typography>
+                <Typography variant="h2" sx={{ marginTop: 3, fontWeight: "bold" }}>
+                    ようこそ！
+                </Typography>
+                <Typography paragraph sx={{ marginTop: 3, marginLeft: 2, marginRight: 5 }}>
+                    Most of the work on this site has been on the Genki 1 chapter study section. Please follow the link
+                    <Link href="/genki/1/study"> here</Link> or click through the navigation menu to get there. Thank
+                    you for your patience as this site is being built out.
+                </Typography>
             </Box>
-            Home Page!
         </div>
     );
 }
