6dacaddcd90378f6dcb480cc9e78052bcefd3647
Cake <kramos577@gmail.com>
2023-07-23 17:23:17 -0700

    Update Genki Base Page with Basic Info

--- frontend/src/pages/Genki.tsx
+++ frontend/src/pages/Genki.tsx
@@ -3,11 +3,15 @@ import { Box, Toolbar, Typography } from "@mui/material";
 function Genki() {
     return (
         <div>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Box component="main" sx={{ width: "85%", float: "right" }}>
                 <Toolbar />
-                <Typography paragraph>This is where all the learning will go!</Typography>
+                <Typography paragraph sx={{ marginTop: 3, marginLeft: 2, marginRight: 5 }}>
+                    Let's study Genki! These Genki study pages are not meant to replace the Genki textbooks but to be
+                    used in conjunction with them. The chapter sections will contain shorter summaries and different
+                    examples than what are found in the textbooks. If you would like to purchase them, you can easily
+                    find the textbooks on the web, both physical and digital copies.
+                </Typography>
             </Box>
-            Genki Base Page!
         </div>
     );
 }
