8e933d52d8cc7e1267b410b8ebc6864bff1ad358
Cake <kramos577@gmail.com>
2023-07-23 17:26:40 -0700

    Updated Genki 1 Base Page with Basic Info

--- frontend/src/pages/Genki1.tsx
+++ frontend/src/pages/Genki1.tsx
@@ -3,9 +3,14 @@ import { Box, Toolbar, Typography } from "@mui/material";
 function Genki1() {
     return (
         <div>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Box component="main" sx={{ width: "85%", float: "right" }}>
                 <Toolbar />
-                <Typography paragraph>Genki I Base Page!</Typography>
+                <Typography paragraph sx={{ marginTop: 3, marginLeft: 2, marginRight: 5 }}>
+                    Let's study Genki I! The Genki I study page is not meant to replace the textbook but to be used in
+                    conjunction with it. The chapter sections will contain shorter summaries and different examples than
+                    what are found in the textbooks. However, a lot of the language is the same. If you would like to
+                    purchase it, you can easily find the textbook on the web, both physical and digital copies.
+                </Typography>
             </Box>
         </div>
     );
