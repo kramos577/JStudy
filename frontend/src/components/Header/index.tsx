a3cc608616a0087b69d219b7c3e4e888b2206cef
Cake <kramos577@gmail.com>
2023-08-14 12:16:15 -0700

    Added Daruma Logo to Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import redDaruma from "../../static/daruma/daruma_red.png";
 
 const style = {
     background: "#02142B",
@@ -10,7 +11,8 @@ function Header() {
     return (
         <AppBar position="fixed" sx={{ ...style, zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar>
-                <Typography variant="h6" noWrap component="div">
+                <img src={redDaruma} alt="daruma logo" width={40} height={40} />
+                <Typography variant="h6" noWrap component="div" sx={{ ml: 1 }}>
                     JStudy
                 </Typography>
             </Toolbar>
