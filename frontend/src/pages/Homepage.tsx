838b9242dc243938f61b3696b67c89dc8b134571
Cake <kramos577@gmail.com>
2023-08-14 12:29:32 -0700

    Add Neko Image to Home Page

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import { Box, Link, Toolbar, Typography } from "@mui/material";
+import blueNeko from "../static/neko/neko_blue.png";
 
 function Homepage() {
     return (
@@ -8,6 +9,7 @@ function Homepage() {
                 <Typography variant="h2" sx={{ marginTop: 3, fontWeight: "bold" }}>
                     ようこそ！
                 </Typography>
+                <img src={blueNeko} alt="neko logo" width={300} height={375} />
                 <Typography paragraph sx={{ marginTop: 3, marginLeft: 2, marginRight: 5 }}>
                     Most of the work on this site has been on the Genki 1 chapter study section. Please follow the link
                     <Link href="/genki/1/study"> here</Link> or click through the navigation menu to get there. Thank
