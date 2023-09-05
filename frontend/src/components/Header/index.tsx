3aeaaab32861078af9f6605b26f80326394de370
Cake <kramos577@gmail.com>
2023-09-05 12:49:50 -0700

    Daruma Icon Redirects to Home Page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import redDaruma from "../../static/daruma/daruma_red.png";
+import { Link } from "react-router-dom";
 
 const style = {
     background: "#02142B",
@@ -11,7 +12,10 @@ function Header() {
     return (
         <AppBar position="fixed" sx={{ ...style, zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar>
-                <img src={redDaruma} alt="daruma logo" width={40} height={40} />
+                <Link to="/">
+                    <img src={redDaruma} alt="daruma logo" width={40} height={40} />
+                </Link>
+
                 <Typography variant="h6" noWrap component="div" sx={{ ml: 1 }}>
                     JStudy
                 </Typography>
