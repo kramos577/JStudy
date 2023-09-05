d99db73fb869e3dbb10010a43487605462006102
Cake <kramos577@gmail.com>
2023-09-05 13:08:40 -0700

    Added Genki 1 Content Template for All Chapters
    The goal is to fill these in and then add them to the study page as
    content is completed.

---
+++ frontend/src/content/genki/genki1/ch12.ts
@@ -0,0 +1,28 @@
+import { ChapterContent } from "../types";
+
+const content: ChapterContent = {
+    chapter: "Chapter 12",
+    isEarly: false,
+    title: {
+        english: "",
+        japanese: { kana: "", japanese: "" },
+    },
+    vocabulary: [
+        {
+            english: "",
+            japanese: "",
+            kana: "",
+        },
+    ],
+    grammar: [
+        {
+            title: "",
+            meanings: [{ english: "", japanese: { kana: "" } }],
+            usages: [""],
+            notes: [""],
+            examples: ["Examples to come"],
+        },
+    ],
+};
+
+export default content;
