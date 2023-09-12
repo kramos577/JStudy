1d9cc8a552aec2ffef237360c7b8285896422fa0
Cake <kramos577@gmail.com>
2023-09-12 14:09:35 -0700

    Add Ch 5 Vocabulary

--- frontend/src/content/genki/genki1/ch5.ts
+++ frontend/src/content/genki/genki1/ch5.ts
@@ -4,14 +4,284 @@ const content: ChapterContent = {
     chapter: "Chapter 5",
     isEarly: false,
     title: {
-        english: "",
-        japanese: { kana: "", japanese: "" },
+        english: "A Trip to Okinawa",
+        japanese: { kana: "おきなわりょこう", japanese: "沖縄旅行" },
     },
     vocabulary: [
         {
-            english: "",
-            japanese: "",
-            kana: "",
+            english: "sea",
+            japanese: "海",
+            kana: "うみ",
+        },
+        {
+            english: "postal stamps",
+            japanese: "切手",
+            kana: "きって",
+        },
+        {
+            english: "ticket",
+            japanese: "切符",
+            kana: "きっぷ",
+        },
+        {
+            english: "surfing",
+            japanese: "サーフィン",
+            kana: "サーフィン",
+        },
+        {
+            english: "homework",
+            japanese: "宿題",
+            kana: "しゅくだい",
+        },
+        {
+            english: "food",
+            japanese: "食べ物",
+            kana: "たべもの",
+        },
+        {
+            english: "birthday",
+            japanese: "誕生日",
+            kana: "たんじょうび",
+        },
+        {
+            english: "test",
+            japanese: "テスト",
+            kana: "テスト",
+        },
+        {
+            english: "weather",
+            japanese: "天気",
+            kana: "てんき",
+        },
+        {
+            english: "drink",
+            japanese: "飲み物",
+            kana: "のみもの",
+        },
+        {
+            english: "postcard",
+            japanese: "葉書",
+            kana: "はがき",
+        },
+        {
+            english: "bus",
+            japanese: "バス",
+            kana: "バス",
+        },
+        {
+            english: "airplane",
+            japanese: "飛行機",
+            kana: "ひこうき",
+        },
+        {
+            english: "room",
+            japanese: "部屋",
+            kana: "へや",
+        },
+        {
+            english: "I (used by men)",
+            japanese: "僕",
+            kana: "ぼく",
+        },
+        {
+            english: "holiday; day off; absence",
+            japanese: "休み",
+            kana: "やすみ",
+        },
+        {
+            english: "travel",
+            japanese: "旅行",
+            kana: "りょこう",
+        },
+        {
+            english: "new",
+            japanese: "新しい",
+            kana: "あたらしい",
+        },
+        {
+            english: "hot (weather)",
+            japanese: "厚い",
+            kana: "あつい",
+        },
+        {
+            english: "hot (thing)",
+            japanese: "熱い",
+            kana: "あつい",
+        },
+        {
+            english: "busy (people/days)",
+            japanese: "忙しい",
+            kana: "いそがしい",
+        },
+        {
+            english: "large",
+            japanese: "大きい",
+            kana: "おおきい",
+        },
+        {
+            english: "interesting; funny",
+            japanese: "面白い",
+            kana: "おもしろい",
+        },
+        {
+            english: "good-looking (conjugates like いい)",
+            japanese: "かっこいい",
+            kana: "かっこいい",
+        },
+        {
+            english: "frightening",
+            japanese: "怖い",
+            kana: "こわい",
+        },
+        {
+            english: "cold (weather - not used for things)",
+            japanese: "寒い",
+            kana: "さむい",
+        },
+        {
+            english: "fun",
+            japanese: "楽しい",
+            kana: "たのしい",
+        },
+        {
+            english: "small",
+            japanese: "小さい",
+            kana: "ちいさい",
+        },
+        {
+            english: "boring",
+            japanese: "つまらない",
+            kana: "つまらない",
+        },
+        {
+            english: "old (thing - not used for people)",
+            japanese: "古い",
+            kana: "ふるい",
+        },
+        {
+            english: "difficult",
+            japanese: "難しい",
+            kana: "むずかしい",
+        },
+        {
+            english: "easy (problem); kind (person)",
+            japanese: "やさしい",
+            kana: "やさしい",
+        },
+        {
+            english: "inexpensive; cheap (thing)",
+            japanese: "安い",
+            kana: "やすい",
+        },
+        {
+            english: "disgusted with; to dislike (~が)",
+            japanese: "嫌い",
+            kana: "きらい（な）",
+        },
+        {
+            english: "beautiful; clean",
+            japanese: "きれい",
+            kana: "きれい（な）",
+        },
+        {
+            english: "healthy; energetic",
+            japanese: "元気",
+            kana: "げんき（な）",
+        },
+        {
+            english: "quiet",
+            japanese: "静か",
+            kana: "しずか（な）",
+        },
+        {
+            english: "fond of; to like (~が)",
+            japanese: "好き",
+            kana: "好き（な）",
+        },
+        {
+            english: "to hate (~)",
+            japanese: "大嫌い",
+            kana: "だいきらい（な）",
+        },
+        {
+            english: "very fond of; to love (~が)",
+            japanese: "大好き",
+            kana: "だいすき（な）",
+        },
+        {
+            english: "lively",
+            japanese: "にぎやか",
+            kana: "にぎやか（な）",
+        },
+        {
+            english: "not busy; to have a lot of free time",
+            japanese: "暇",
+            kana: "ひま（な）",
+        },
+        {
+            english: "to swim",
+            japanese: "泳ぐ",
+            kana: "およぐ",
+        },
+        {
+            english: "to ask (person に)",
+            japanese: "聞く",
+            kana: "きく",
+        },
+        {
+            english: "to ride; to board (~に)",
+            japanese: "乗る",
+            kana: "のる",
+        },
+        {
+            english: "to do; to perform (~を)",
+            japanese: "やる",
+            kana: "やる",
+        },
+        {
+            english: "to go out",
+            japanese: "出かける",
+            kana: "でかける",
+        },
+        {
+            english: "together",
+            japanese: "一緒に",
+            kana: "いっしょに",
+        },
+        {
+            english: "extremely",
+            japanese: "すごく",
+            kana: "すごく",
+        },
+        {
+            english: "and then",
+            japanese: "それから",
+            kana: "それから",
+        },
+        {
+            english: "It's okay.; Not to worry.; Everything is under control.",
+            japanese: "大丈夫",
+            kana: "だいじょうぶ",
+        },
+        {
+            english: "very",
+            japanese: "とても",
+            kana: "とても",
+        },
+        {
+            english: "what kind of...",
+            japanese: "どんな",
+            kana: "どんな",
+        },
+        {
+            english: "[counter for flat objects]",
+            japanese: "〜枚",
+            kana: "〜まい",
+        },
+        {
+            english: "to (a place); as far as (a place); till (a time)",
+            japanese: "〜まで",
+            kana: "〜まで",
         },
     ],
     grammar: [
