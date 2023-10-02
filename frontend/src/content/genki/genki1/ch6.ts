2f7853cbcd29c9d15780443dfe3965f69dcc5378
Cake <kramos577@gmail.com>
2023-10-02 13:27:41 -0700

    Added Ch 6 Vocab Content

--- frontend/src/content/genki/genki1/ch6.ts
+++ frontend/src/content/genki/genki1/ch6.ts
@@ -4,14 +4,264 @@ const content: ChapterContent = {
     chapter: "Chapter 6",
     isEarly: false,
     title: {
-        english: "",
-        japanese: { kana: "", japanese: "" },
+        english: "A Day in Robert's Life",
+        japanese: { kana: "ロバートさんのいちにち", japanese: "ロバートさんの一日" },
     },
     vocabulary: [
         {
-            english: "",
-            japanese: "",
-            kana: "",
+            english: "money",
+            japanese: "お金",
+            kana: "おかね",
+        },
+        {
+            english: "bath",
+            japanese: "お風呂",
+            kana: "おふろ",
+        },
+        {
+            english: "kanji; Chinese character",
+            japanese: "漢字",
+            kana: "かんじ",
+        },
+        {
+            english: "textbook",
+            japanese: "教科書",
+            kana: "きょうかしょ",
+        },
+        {
+            english: "this week",
+            japanese: "今週",
+            kana: "こんしゅう",
+        },
+        {
+            english: "CD",
+            japanese: "シーディー",
+            kana: "シーディー",
+        },
+        {
+            english: "municipal hospital",
+            japanese: "市民病院",
+            kana: "しみんびょういん",
+        },
+        {
+            english: "shower",
+            japanese: "シャワー",
+            kana: "シャワー",
+        },
+        {
+            english: "next",
+            japanese: "次",
+            kana: "つぎ",
+        },
+        {
+            english: "electricity",
+            japanese: "電気",
+            kana: "でんき",
+        },
+        {
+            english: "train",
+            japanese: "電車",
+            kana: "でんしゃ",
+        },
+        {
+            english: "baggage",
+            japanese: "荷物",
+            kana: "にもつ",
+        },
+        {
+            english: "personal computer",
+            japanese: "パソコン",
+            kana: "パソコン",
+        },
+        {
+            english: "page",
+            japanese: "ページ",
+            kana: "ページ",
+        },
+        {
+            english: "window",
+            japanese: "窓",
+            kana: "まど",
+        },
+        {
+            english: "night",
+            japanese: "夜",
+            kana: "よる",
+        },
+        {
+            english: "next week",
+            japanese: "来週",
+            kana: "らいしゅう",
+        },
+        {
+            english: "next year",
+            japanese: "来年",
+            kana: "らいねん",
+        },
+        {
+            english: "tough (situation)",
+            japanese: "大変（な）",
+            kana: "たいへん（な）",
+        },
+        {
+            english: "to play; to spend time pleasantly",
+            japanese: "遊ぶ",
+            kana: "あそぶ",
+        },
+        {
+            english: "to hurry",
+            japanese: "急ぐ",
+            kana: "いそぐ",
+        },
+        {
+            english: "to take a bath",
+            japanese: "お風呂に入る",
+            kana: "おふろにはいる",
+        },
+        {
+            english: "to return (a thing) (person に thing を)",
+            japanese: "返す",
+            kana: "かえす",
+        },
+        {
+            english: "to turn off; to erase (~を)",
+            japanese: "消す",
+            kana: "けす",
+        },
+        {
+            english: "to die",
+            japanese: "死ぬ",
+            kana: "しぬ",
+        },
+        {
+            english: "to sit down (seat に)",
+            japanese: "座る",
+            kana: "すわる",
+        },
+        {
+            english: "to stand up",
+            japanese: "立つ",
+            kana: "たつ",
+        },
+        {
+            english: "to smoke",
+            japanese: "たばこを吸う",
+            kana: "たばこをすう",
+        },
+        {
+            english: "to use (~を)",
+            japanese: "使う",
+            kana: "つかう",
+        },
+        {
+            english: "to help (person/task を)",
+            japanese: "手伝う",
+            kana: "てつだう",
+        },
+        {
+            english: "to enter (~に)",
+            japanese: "入る",
+            kana: "はいる",
+        },
+        {
+            english: "to carry; to hold (~を)",
+            japanese: "持つ",
+            kana: "もつ",
+        },
+        {
+            english: "to be absent (from...) (~を); to rest",
+            japanese: "休む",
+            kana: "やすむ",
+        },
+        {
+            english: "to open (something) (~を)",
+            japanese: "開ける",
+            kana: "あける",
+        },
+        {
+            english: "to teach; to instruct (person に thing を)",
+            japanese: "教える",
+            kana: "おしえる",
+        },
+        {
+            english: "to get off (~を)",
+            japanese: "降りる",
+            kana: "おりる",
+        },
+        {
+            english: "to borrow (person に thing を)",
+            japanese: "借りる",
+            kana: "かりる",
+        },
+        {
+            english: "to close (something) (~を)",
+            japanese: "閉める",
+            kana: "しめる",
+        },
+        {
+            english: "to take a shower",
+            japanese: "シャワーを浴びる",
+            kana: "シャワーをあびる",
+        },
+        {
+            english: "to turn on (~を)",
+            japanese: "つける",
+            kana: "つける",
+        },
+        {
+            english: "to make a phone call (~に)",
+            japanese: "電話をかける",
+            kana: "でんわをかける",
+        },
+        {
+            english: "to forget; to leave behind (~を)",
+            japanese: "忘れる",
+            kana: "わすれる",
+        },
+        {
+            english: "to bring (a person) (~を)",
+            japanese: "連れてくる",
+            kana: "つれてくる",
+        },
+        {
+            english: "to bring (a thing) (~を)",
+            japanese: "持ってくる",
+            kana: "もってくる",
+        },
+        {
+            english: "later on",
+            japanese: "後で",
+            kana: "あとで",
+        },
+        {
+            english: "(do something) late",
+            japanese: "遅く",
+            kana: "おそく",
+        },
+        {
+            english: "because ...",
+            japanese: "〜から",
+            kana: "〜から",
+        },
+        {
+            english: "That would be fine.; That wouldn't be necessary",
+            japanese: "結構です",
+            kana: "けっこうです",
+        },
+        {
+            english: "right away",
+            japanese: "すぐ",
+            kana: "すぐ",
+        },
+        {
+            english: "Really?",
+            japanese: "本当ですか",
+            kana: "ほんとうですか",
+        },
+        {
+            english: "slowly; leisurely; unhurriedly",
+            japanese: "ゆっくり",
+            kana: "ゆっくり",
         },
     ],
     grammar: [
