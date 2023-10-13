4c3de0032d57ad50ffda8e784099de66126a465e
Cake <kramos577@gmail.com>
2023-10-13 13:20:38 -0700

    Add Genki Ch 6 Content to Study Page

--- frontend/src/pages/Genki1Study.tsx
+++ frontend/src/pages/Genki1Study.tsx
@@ -4,11 +4,12 @@ import contentCh2 from "../content/genki/genki1/ch2";
 import contentCh3 from "../content/genki/genki1/ch3";
 import contentCh4 from "../content/genki/genki1/ch4";
 import contentCh5 from "../content/genki/genki1/ch5";
+import contentCh6 from "../content/genki/genki1/ch6";
 import Vocab from "../components/Vocab";
 import Grammar from "../components/Grammar";
 import { Link } from "react-router-dom";
 
-const contentGenki1 = [contentCh1, contentCh2, contentCh3, contentCh4, contentCh5];
+const contentGenki1 = [contentCh1, contentCh2, contentCh3, contentCh4, contentCh5, contentCh6];
 
 function Genki1Study() {
     return (
