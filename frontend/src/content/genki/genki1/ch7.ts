2a2bd1257d785257c39805b33f4899212fc0955e
Cake <kramos577@gmail.com>
2023-10-16 15:54:06 -0700

    Added Genki Ch 7 Vocab

--- frontend/src/content/genki/genki1/ch7.ts
+++ frontend/src/content/genki/genki1/ch7.ts
@@ -4,14 +4,309 @@ const content: ChapterContent = {
     chapter: "Chapter 7",
     isEarly: false,
     title: {
-        english: "",
-        japanese: { kana: "", japanese: "" },
+        english: "Family Picture",
+        japanese: { kana: "かぞくのしゃしん", japanese: "家族の写真" },
     },
     vocabulary: [
         {
-            english: "",
-            japanese: "",
-            kana: "",
+            english: "(my) older sister",
+            japanese: "姉",
+            kana: "あね",
+        },
+        {
+            english: "apartment",
+            japanese: "アパート",
+            kana: "アパート",
+        },
+        {
+            english: "younger sister",
+            japanese: "妹",
+            kana: "いもうと",
+        },
+        {
+            english: "song",
+            japanese: "歌",
+            kana: "うた",
+        },
+        {
+            english: "grandfather; old man",
+            japanese: "おじいさん",
+            kana: "おじいさん",
+        },
+        {
+            english: "younger brother",
+            japanese: "弟",
+            kana: "おとうと",
+        },
+        {
+            english: "man",
+            japanese: "男の人",
+            kana: "おとこのひと",
+        },
+        {
+            english: "older brother",
+            japanese: "お兄さん",
+            kana: "おにいさん",
+        },
+        {
+            english: "older sister",
+            japanese: "お姉さん",
+            kana: "おねえさん",
+        },
+        {
+            english: "grandmother; old woman",
+            japanese: "おばあさん",
+            kana: "おばあさん",
+        },
+        {
+            english: "woman",
+            japanese: "女の人",
+            kana: "おんあのひと",
+        },
+        {
+            english: "company",
+            japanese: "会社",
+            kana: "かいしゃ",
+        },
+        {
+            english: "family",
+            japanese: "家族",
+            kana: "かぞく",
+        },
+        {
+            english: "hair",
+            japanese: "髪",
+            kana: "かみ",
+        },
+        {
+            english: "brothers and sisters",
+            japanese: "兄弟",
+            kana: "きょうだい",
+        },
+        {
+            english: "mouth",
+            japanese: "口",
+            kana: "くち",
+        },
+        {
+            english: "country; place of origin",
+            japanese: "国",
+            kana: "くに",
+        },
+        {
+            english: "car",
+            japanese: "車",
+            kana: "くるま",
+        },
+        {
+            english: "game",
+            japanese: "ゲーム",
+            kana: "ゲーム",
+        },
+        {
+            english: "convience store",
+            japanese: "コンビニ",
+            kana: "コンビニ",
+        },
+        {
+            english: "club activity",
+            japanese: "サークル",
+            kana: "サークル",
+        },
+        {
+            english: "cafeteria; dining commons",
+            japanese: "食堂",
+            kana: "しょくどう",
+        },
+        {
+            english: "(my) father",
+            japanese: "父",
+            kana: "ちち",
+        },
+        {
+            english: "DVD",
+            japanese: "ディーブイディー",
+            kana: "ディーブイディー",
+        },
+        {
+            english: "(my) mother",
+            japanese: "母",
+            kana: "はは",
+        },
+        {
+            english: "eye",
+            japanese: "目",
+            kana: "め",
+        },
+        {
+            english: "glasses",
+            japanese: "眼鏡",
+            kana: "めがね",
+        },
+        {
+            english: "bright; smart; clever",
+            japanese: "頭がいい",
+            kana: "あたまがいい",
+        },
+        {
+            english: "cute",
+            japanese: "かわいい",
+            kana: "かわいい",
+        },
+        {
+            english: "tall (stature)",
+            japanese: "背が高い",
+            kana: "せがたかい",
+        },
+        {
+            english: "short (stature)",
+            japanese: "背が低い",
+            kana: "せがひくい",
+        },
+        {
+            english: "long",
+            japanese: "長い",
+            kana: "ながい",
+        },
+        {
+            english: "fast",
+            japanese: "速い",
+            kana: "はやい",
+        },
+        {
+            english: "short (length)",
+            japanese: "短い",
+            kana: "みじかい",
+        },
+        {
+            english: "kind",
+            japanese: "親切",
+            kana: "しんせつ（な）",
+        },
+        {
+            english: "convenient",
+            japanese: "便利",
+            kana: "べんり（な）",
+        },
+        {
+            english: "to sing (~を)",
+            japanese: "歌う",
+            kana: "うたう",
+        },
+        {
+            english: "to put on (a hat) (~を)",
+            japanese: "かぶる",
+            kana: "かぶる",
+        },
+        {
+            english: "to get to know (~を)",
+            japanese: "知る",
+            kana: "しる",
+        },
+        {
+            english: "I know",
+            japanese: "知っています",
+            kana: "しっています",
+        },
+        {
+            english: "I do not know",
+            japanese: "知りません",
+            kana: "しりません",
+        },
+        {
+            english: "to live (~に)",
+            japanese: "住む",
+            kana: "すむ",
+        },
+        {
+            english: "to put on (items below your waist) (~を)",
+            japanese: "はく",
+            kana: "はく",
+        },
+        {
+            english: "to gain weight",
+            japanese: "太る",
+            kana: "ふとる",
+        },
+        {
+            english: "to be on the heavy side",
+            japanese: "太っています",
+            kana: "ふとっています",
+        },
+        {
+            english: "to put on (glasses)",
+            japanese: "（めがねを）かける",
+            kana: "（めがねを）かける",
+        },
+        {
+            english: "to put on (clothes above your waist) (~を)",
+            japanese: "着る",
+            kana: "きる",
+        },
+        {
+            english: "to work for (~に)",
+            japanese: "勤める",
+            kana: "つとめる",
+        },
+        {
+            english: "to lose weight",
+            japanese: "やせる",
+            kana: "やせる",
+        },
+        {
+            english: "to be thin",
+            japanese: "やせています",
+            kana: "やせています",
+        },
+        {
+            english: "to get married (~と)",
+            japanese: "結婚する",
+            kana: "けっこんする",
+        },
+        {
+            english: "..., but",
+            japanese: "〜が",
+            kana: "〜が",
+        },
+        {
+            english: "not...anything",
+            japanese: "何も。。。ない",
+            kana: "なにも。。。ない",
+        },
+        {
+            english: "counter for people",
+            japanese: "人",
+            kana: "にん",
+        },
+        {
+            english: "one person",
+            japanese: "一人",
+            kana: "ひとり",
+        },
+        {
+            english: "two people",
+            japanese: "二人",
+            kana: "ふたり",
+        },
+        {
+            english: "nothing in particular",
+            japanese: "別に。。。ない",
+            kana: "べつに。。。ない",
+        },
+        {
+            english: "Hello? (used on the phone)",
+            japanese: "もしもし",
+            kana: "もしもし",
+        },
+        {
+            english: "of course",
+            japanese: "もちろん",
+            kana: "もちろん",
+        },
+        {
+            english: "if you like",
+            japanese: "よかったら",
+            kana: "よかったら",
         },
     ],
     grammar: [
