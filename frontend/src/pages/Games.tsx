225cf84e392a68201099cfdb8a5b2a3d4c1f59c7
Cake <kramos577@gmail.com>
2023-10-16 16:42:05 -0700

    Add Games Page Basic Cards
    All the cards say the games are under construction, and there are
    some TODOs to address later as development progresses

--- frontend/src/pages/Games.tsx
+++ frontend/src/pages/Games.tsx
@@ -1,4 +1,8 @@
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Toolbar, Typography } from "@mui/material";
+import ConstructionIcon from "@mui/icons-material/Construction";
+
+// TODO: Make a new type for these that include name, path, summary
+const GAME_NAMES = ["Flash Cards", "Crosswords", "Scrabble", "Hangman", "Matching", "Bingo"];
 
 function Games() {
     return (
@@ -9,6 +13,31 @@ function Games() {
                     Let's study Japanese through games!
                 </Typography>
             </Box>
+            <Grid container spacing={2} sx={{ width: "82%", float: "right" }}>
+                {GAME_NAMES.map((gameName) => (
+                    <Grid item>
+                        {/* Add card actions once a game is working */}
+                        <Card sx={{ width: 350, backgroundColor: "#f8fdff", border: 1.5 }} variant="outlined">
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {gameName}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    This game is under construction. Thank you for waiting.
+                                </Typography>
+                                <ConstructionIcon
+                                    sx={{
+                                        color: "#f57c00",
+                                        width: 50,
+                                        height: 50,
+                                        mt: 1,
+                                    }}
+                                />
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     );
 }
