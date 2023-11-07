9c7e30447075dd34991500d3cc83fe84ab458883
Cake <kramos577@gmail.com>
2023-11-07 14:33:16 -0800

    Add Table of Contents to Genki 1 Page

--- frontend/src/pages/Genki1.tsx
+++ frontend/src/pages/Genki1.tsx
@@ -1,9 +1,11 @@
 import { Box, Toolbar, Typography } from "@mui/material";
+import TableOfContents from "../components/TableOfContents";
+import greenDaruma from "../static/daruma/daruma_green.png";
 
 function Genki1() {
     return (
         <div>
-            <Box>
+            <Box display="flex" sx={{ flexDirection: "column", mb: 3 }} justifyContent="center" alignItems="center">
                 <Toolbar />
                 <Typography paragraph sx={{ marginTop: 3, marginLeft: 2, marginRight: 5 }}>
                     Let's study Genki I! The Genki I study page is not meant to replace the textbook but to be used in
@@ -11,6 +13,10 @@ function Genki1() {
                     what are found in the textbooks. However, a lot of the language is the same. If you would like to
                     purchase it, you can easily find the textbook on the web, both physical and digital copies.
                 </Typography>
+                <img src={greenDaruma} alt="neko logo" width={150} height={150} />
+                <Box width="80%">
+                    <TableOfContents />
+                </Box>
             </Box>
         </div>
     );
